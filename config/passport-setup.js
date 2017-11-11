bcae3bddfaa3f835725c8bdf49a26c4cbc722280
Robert Castiblanco <ccrs2006@gmail.com>
2017-11-10 19:55:00 -0700

    ready2

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -17,7 +17,7 @@ passport.deserializeUser((id, done) => {
 passport.use(
     new GoogleStrategy({
         //options for the google strategy
-        callbackURL: '/auth/google/redirect',
+        callbackURL: 'https://recipe-matching.herokuapp.com/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
